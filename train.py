21c7e5d58ecd3e1a14ebe20a0a1a1adf0eee6f71
whoisjones <jonas.golde@gmail.com>
2020-07-28 19:00:34 +0200

    spacy model

--- train.py
+++ train.py
@@ -1,15 +1,8 @@
 import fasttext
 
 def train():
-    with open('resources/en-wikipedia.tokenized.txt', "r") as f:
-        text = f.read()
-        text.lower()
-
-        with open("resources/en-wikipedia.tokenized.lowercase.txt") as out:
-            out.write(text)
-
-    model = fasttext.train_unsupervised('resources/en-wikipedia.tokenized.lowercase.txt', "cbow", dim=32)
-    model.save_model("resources/fasttext_wikipedia_normal.bin")
+    model = fasttext.train_unsupervised('resources/resources/en-wikipedia.tokenized.spacy-grouped.txt', "cbow", dim=32)
+    model.save_model("resources/fasttext_wikipedia_spacy.bin")
 
 
 
