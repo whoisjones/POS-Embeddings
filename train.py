0485af54681f85227ce906f5f848fb7889aa996e
whoisjones <jonas.golde@gmail.com>
2020-07-28 14:08:32 +0200

    train script

--- train.py
+++ train.py
@@ -1,8 +1,7 @@
 import fasttext
-from glove import Corpus, Glove
 
 def train():
-    model = fasttext.train_unsupervised('masked_tags.txt', "cbow", dim=32)
+    model = fasttext.train_unsupervised('resources/en-wikipedia.tokenized.txt', "cbow", dim=32)
     model.save_model("resources/fasttext_wikipedia_normal.bin")
 
 
