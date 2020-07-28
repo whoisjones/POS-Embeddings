503c05485ebfbf3a5d571380981382022e36b986
whoisjones <jonas.golde@gmail.com>
2020-07-28 19:02:21 +0200

    spacy model

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import fasttext
 
 def train():
-    model = fasttext.train_unsupervised("resources/resources/en-wikipedia.tokenized.spacy-grouped.txt'", "cbow", dim=32)
+    model = fasttext.train_unsupervised("resources/en-wikipedia.tokenized.spacy-grouped.txt'", "cbow", dim=32)
     model.save_model("resources/fasttext_wikipedia_spacy.bin")
 
 
