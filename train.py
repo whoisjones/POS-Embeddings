ae68366b6c94ef914c1941f574fb543c7475fd39
whoisjones <jonas.golde@gmail.com>
2020-07-29 09:22:03 +0200

    normal model

--- train.py
+++ train.py
@@ -1,10 +1,8 @@
 import fasttext
 
 def train():
-    model = fasttext.train_unsupervised("resources/en-wikipedia.tokenized.spacy-grouped.txt'", "cbow", dim=32)
-    model.save_model("resources/fasttext_wikipedia_spacy.bin")
-
-
+    model = fasttext.train_unsupervised("resources/en-wikipedia.tokenized.txt'", "cbow", dim=32)
+    model.save_model("resources/fasttext_wikipedia_normal.bin")
 
 if __name__ == "__main__":
     train()
