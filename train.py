1ff89f32a1d21f0670a4aa8b1a2ad38983dcca22
whoisjones <jonas.golde@gmail.com>
2020-07-30 13:45:51 +0200

    changed params

--- train.py
+++ train.py
@@ -1,8 +1,14 @@
 import fasttext
 
 def train():
-    model = fasttext.train_unsupervised("resources/en-wikipedia.tokenized.txt", "cbow", dim=32)
-    model.save_model("resources/fasttext_wikipedia_normal.bin")
+    for dim in [25, 100]:
+        model = fasttext.train_unsupervised("resources/en-wikipedia.tokenized.spacy-grouped.txt", "skipgram", minn=0, maxn=0, dim=dim)
+        model.save_model(f"resources/fasttext_wikipedia_spacy_{dim}dim.bin")
+
+    for dim in [25, 100, 400]:
+        model = fasttext.train_unsupervised("resources/en-wikipedia.tokenized.txt", "skipgram", minn=0, maxn=0, dim=dim)
+        model.save_model(f"resources/fasttext_wikipedia_normal_{dim}dim.bin")
+
 
 if __name__ == "__main__":
     train()
