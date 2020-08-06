537e15e1babe796399f101818b14ed49daa69175
whoisjones <jonas.golde@gmail.com>
2020-08-06 11:28:53 +0200

    changed verbs regex

--- regex.py
+++ regex.py
@@ -12,12 +12,12 @@ def main():
         text = text.replace("_nn", "_N")
 
         # verbs
-        text = text.replace("_vb", "_V")
         text = text.replace("_vbd", "_V")
         text = text.replace("_vbg", "_V")
         text = text.replace("_vbn", "_V")
         text = text.replace("_vbp", "_V")
         text = text.replace("_vbz", "_V")
+        text = text.replace("_vb", "_V")
 
         # rest
         text = text.replace("_add", "_REST")
@@ -30,28 +30,28 @@ def main():
         text = text.replace("_gw", "_REST")
         text = text.replace("_hyph", "_REST")
         text = text.replace("_in", "_REST")
-        text = text.replace("_jj", "_REST")
         text = text.replace("_jjr", "_REST")
         text = text.replace("_jjs", "_REST")
+        text = text.replace("_jj", "_REST")
         text = text.replace("_ls", "_REST")
         text = text.replace("_md", "_REST")
         text = text.replace("_nfp", "_REST")
         text = text.replace("_nil", "_REST")
         text = text.replace("_pdt", "_REST")
         text = text.replace("_pos", "_REST")
-        text = text.replace("_prp", "_REST")
         text = text.replace("_prp$", "_REST")
-        text = text.replace("_rb", "_REST")
+        text = text.replace("_prp", "_REST")
         text = text.replace("_rbr", "_REST")
         text = text.replace("_rbs", "_REST")
+        text = text.replace("_rb", "_REST")
         text = text.replace("_rp", "_REST")
         text = text.replace("_sp", "_REST")
         text = text.replace("_sym", "_REST")
         text = text.replace("_to", "_REST")
         text = text.replace("_uh", "_REST")
         text = text.replace("_wdt", "_REST")
-        text = text.replace("_wp", "_REST")
         text = text.replace("_wp$", "_REST")
+        text = text.replace("_wp", "_REST")
         text = text.replace("_wrb", "_REST")
         text = text.replace("_xx", "_REST")
         text = text.replace("__sp", "_REST")
@@ -64,7 +64,7 @@ def main():
         text = text.replace("_``", "_REST")
         text = text.replace("_$", "_REST")
 
-    with open("resources/en-wikipedia.tokenized.lowercased.spacy-grouped.txt'", "w") as out:
+    with open("resources/en-wikipedia.tokenized.lowercased.spacy-grouped.txt", "w") as out:
         out.write(text)
 
 if __name__ == "__main__":
